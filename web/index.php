3fd6a58ead665e1178187734fd66c2c757b177d4
Romain Bellina <contact@romainbellina.fr>
2013-02-25 15:42:54 +0100

    Add routes

--- web/index.php
+++ web/index.php
@@ -4,8 +4,31 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello', function() {
-    return 'Hello!';
+$app->get('/', function() {
+    return $app['twig']->render('template/home.twig', array(
+    );
+});
+
+$app->get('/admin', function() {
+    return $app['twig']->render('template/admin.twig', array(
+        );
+});
+
+$app->get('/article', function() {
+    return $app['twig']->render('template/article.twig', array(
+    );
+});
+
+$app->get('/article/{id}', function($id) {
+
+    return $app['twig']->render('template/article_detail.twig', array(
+        'id_article'=>$id,
+    );
+});
+
+$app->get('/contact', function() {
+    return $app['twig']->render('template/contact.twig', array(
+    );
 });
 
 $app->run();
