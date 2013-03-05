97ae6afca2493d9aa89aa92e65584b93400aa980
Romain Bellina <contact@romainbellina.fr>
2013-03-05 11:04:07 +0100

    Update bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -14,6 +14,8 @@ $app = new Silex\Application();
 //Affiche les bugs
 $app['debug']=true;
 
+use Symfony\Component\HttpFoundation\Request;
+
 //Mise en route de twig
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
@@ -32,9 +34,6 @@ $app->register(new Silex\Provider\TranslationServiceProvider(), array(
 $app->register(new Silex\Provider\FormServiceProvider());
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
-$app->register(new Silex\Provider\SwiftmailerServiceProvider());
-
 
 return $app;
 
