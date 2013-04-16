840daf61090a235d1f85c9fa351fb4ce164226fd
ptithom <t.sire41@gmail.com>
2013-04-16 14:35:47 +0200

    menu

--- web/js/fonction.js
+++ web/js/fonction.js
@@ -33,38 +33,9 @@ $("#prev_article").click(function() {
 
 });
                         
-                        
-                        
-$(document).ready(function(){
-        document.getElementById('sous_menu_2').style.display="none";
-        $("#ul_menu a").click(function(){
-                $("#ul_menu ul").css( "margin-left", "292px" );
-                $("#ul_menu ul ul ").css( "margin-left", "0px" );
-                $(this).next().slideToggle("slow");
-                $(this).parent().siblings().find("ul").slideUp();
-                return false;
-        });
-});
-
-
-
-$(document).ready(function () {	
-	$('.sous_menu li').click(
-               
-		function () { alert("coucou");
-			$('ul', this).stop().slideDown(100);
-		}, 
-		function () {
-			$('ul', this).stop().slideUp(100);			
-		}
-	);
-	
-});
-
-
 
 $(document).ready( function(){
-         $('#slideshowHolder').jqFancyTransitions({ width: 291, height: 291, effect: 'wave',navigation: true});
+        $('#slideshowHolder').jqFancyTransitions({ width: 291, height: 291, effect: 'wave',navigation: true});
 });
 
 
@@ -79,3 +50,22 @@ $("#lang_fr").mouseout(function() {   $(this).attr('src', '/Pollina/web/img/fr_f
 $("#lang_de").mouseout(function() {   $(this).attr('src', '/Pollina/web/img/de_false.png'); });
 $("#lang_en").mouseout(function() {   $(this).attr('src', '/Pollina/web/img/en_false.png'); });
 
+
+//menu
+$("#menu2").hover(
+        function () {
+            $("#sousmenuMetier").show();
+        },
+        function () {
+            $("#sousmenuMetier").hide();
+        }
+);
+
+$("#sousmenuMetier").hover(
+        function () {
+            $(this).show();
+        },
+        function () {
+            $(this).hide();
+        }
+);
